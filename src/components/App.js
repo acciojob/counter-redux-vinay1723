d40a1fb076bd9f3f17db9e6c4e71798c50594bfe
vinay1723 <vinaykumar.17july@gmail.com>
2025-09-09 14:46:03 +0530

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
 
 const App = () => {
+  let count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
